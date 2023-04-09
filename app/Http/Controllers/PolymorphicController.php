fd4a3d44dc4b64f2a40b358dd3f14c43cd326fb0
Luis Zelaya <codewithluis11@gmail.com>
2023-04-08 22:47:31 -0600

    Curso Relaciones: Video Many to Many Polymorphic

--- app/Http/Controllers/PolymorphicController.php
+++ app/Http/Controllers/PolymorphicController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Country;
 use App\Models\Hospital;
+use App\Models\Post;
 use App\Models\Profile;
+use App\Models\Tag;
 use App\Models\User;
+use App\Models\Video;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class PolymorphicController extends Controller
@@ -31,6 +35,38 @@ class PolymorphicController extends Controller
 
     public function indexManyMany(): View
     {
+        //Ejemplo 1
+        // $video = Video::create([
+        //     'title' => 'Video 2'
+        // ]);
+        //
+        // $video->tags()->create([
+        //     'name' => 'Canciones'
+        // ]);
+
+        //Ejemplo 2
+        // $tag = Tag::find(2);
+        // //
+        // // $tag->videos()->create([
+        // //     'title' => 'Video 5'
+        // // ]);
+        //
+        // $video = Video::find(4);
+        //
+        // $video->tags()->attach($tag);
+
+
+        //Ejemplo 3
+        // DB::transaction(function () {
+        //     $post = Post::create([
+        //         'title' => 'Post 2'
+        //     ]);
+        //
+        //     $post->tags()->create([
+        //         'name' => 'Facebook'
+        //     ]);
+        // });
+
         return view('polymorphic.many_many');
     }
 }
